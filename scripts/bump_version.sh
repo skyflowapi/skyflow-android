57f7930f4030b73458ee53b4712d3d51b5c5c732
skyflow-tejesh-reddy <86997821+skyflow-tejesh-reddy@users.noreply.github.com>
2022-06-28 17:11:06 +0530

    [SDK-595] Update bump_version script

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -6,14 +6,14 @@ if [ -z $2 ]
 then
 	echo "Bumping package version to $1"
 
-	sed -E "s/mVersionName = .+/mVersionName = \"$SEMVER\"/g" Skyflow/build.gradle > tempfile && cat tempfile > Skyflow/build.gradle && rm -f tempfile
+	sed -E "s/mVersionName = .+/mVersionName = \'$SEMVER\'/g" Skyflow/build.gradle > tempfile && cat tempfile > Skyflow/build.gradle && rm -f tempfile
 	
 	echo --------------------------
 	echo "Done, Package now at $1"
 else
 	echo "Bumping package version to $1-dev.$2"
 
-	sed -E "s/mVersionName = .+/mVersionName = \"$SEMVER-dev.$2\"/g" Skyflow/build.gradle > tempfile && cat tempfile > Skyflow/build.gradle && rm -f tempfile
+	sed -E "s/mVersionName = .+/mVersionName = \'$SEMVER-dev.$2\'/g" Skyflow/build.gradle > tempfile && cat tempfile > Skyflow/build.gradle && rm -f tempfile
 	
 	echo --------------------------
 	echo "Done, Package now at $1-dev.$2"
